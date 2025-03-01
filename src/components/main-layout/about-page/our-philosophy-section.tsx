d73c9ab1857a99d21308c2146f92a708c7d8ed3f
Req <snogedirl@gmail.com>
2025-03-02 00:39:01 +0100

    fix to SectionParagraph prop

--- src/components/main-layout/about-page/our-philosophy-section.tsx
+++ src/components/main-layout/about-page/our-philosophy-section.tsx
@@ -6,13 +6,13 @@ function OurPhilosophySection() {
       <SectionTitle>Our Pizza Philosophy</SectionTitle>
       <div className="flex flex-col gap-6 sm:flex-row-reverse">
         <div className="grid w-full gap-4 sm:w-1/2">
-          <SectionParagraph>
+          <SectionParagraph wrapBalance={true}>
             We believe that the best pizzas are made with love, patience, and
             high-quality ingredients. Our approach combines traditional
             techniques with modern twists, always striving for that perfect
             balance of flavors and textures.
           </SectionParagraph>
-          <SectionParagraph>
+          <SectionParagraph wrapBalance={true}>
             From hand-kneading our dough to sourcing the freshest local
             toppings, every step of our pizza-making process is a labor of love.
             And we're excited to share all we've learned with you!
