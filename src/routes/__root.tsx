5f3977e8c880b80cb7349de678699725b51a667d
Req <snogedirl@gmail.com>
2025-03-10 00:03:32 +0100

    remove TanStackRouterDevtools from root route component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,22 +1,11 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import React from "react";
 import GlobalNotFound from "../components/global-not-found";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
-const TanStackRouterDevtools =
-  import.meta.env.NODE_ENV === "production"
-    ? () => null
-    : React.lazy(() =>
-        import("@tanstack/router-devtools").then((res) => ({
-          default: res.TanStackRouterDevtools,
-        })),
-      );
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
-      <TanStackRouterDevtools />
       <ReactQueryDevtools />
     </>
   ),
