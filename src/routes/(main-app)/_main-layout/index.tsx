ccbf9787b4f2bbf2fdf1980bad3969032e09fe93
Req <snogedirl@gmail.com>
2025-03-10 13:28:35 +0100

    feat: add loading spinner and no posts message in main layout

--- src/routes/(main-app)/_main-layout/index.tsx
+++ src/routes/(main-app)/_main-layout/index.tsx
@@ -2,17 +2,28 @@ import { createFileRoute } from "@tanstack/react-router";
 import Post from "../../../components/main-layout/homepage/Post";
 import MainWrapper from "../../../components/main-layout/main-wrapper";
 import { useFetchPosts } from "../../../hooks/useFetchPosts";
+import { Spinner } from "@/components/spinners";
 
 export const Route = createFileRoute("/(main-app)/_main-layout/")({
   component: Index,
 });
 
 function Index() {
-  const { data: posts = [] } = useFetchPosts();
+  const { data: posts = [], isLoading } = useFetchPosts();
 
   return (
     <MainWrapper maxWidth="max-w-4xl">
       <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-8">
+        {isLoading && (
+          <div>
+            <Spinner />
+          </div>
+        )}
+
+        {posts.length === 0 && !isLoading && (
+          <div className="text-center text-gray-500">No posts found</div>
+        )}
+
         {posts.length > 0 &&
           posts.map((post) => <Post key={post._id} post={post} />)}
       </div>
